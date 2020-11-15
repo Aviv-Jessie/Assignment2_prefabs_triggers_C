9c67b0e65d11c608e732b365f69280b2680cc4d2
aviv1620 <44925669+aviv1620@users.noreply.github.com>
2020-11-15 23:00:32 +0200

    first commit

---
+++ Assets/Scripts/TakeTheBall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeTheBall : MonoBehaviour {
+
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag = "thrower";
+    [Tooltip("key to press")]
+    [SerializeField] protected KeyCode keyToPress;
+
+    const float timeToWait = 0.1f;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == triggeringTag) {           
+            var keyboardSpawnerComponent = other.GetComponent<KeyboardSpawner>();
+            if (keyboardSpawnerComponent) {
+                keyboardSpawnerComponent.StartCoroutine(ShieldTemporarily(keyboardSpawnerComponent));
+            }
+        } else {
+            Debug.Log("doesn't have KeyboardSpawner "+other.name);
+        }
+    }
+    private IEnumerator ShieldTemporarily(KeyboardSpawner keyboardSpawnerComponent) {
+        keyboardSpawnerComponent.enabled = true;
+        bool thrown = false;
+        while (!thrown) {            
+            if(Input.GetKeyDown(keyToPress))
+                thrown = true;
+            yield return new WaitForSeconds(timeToWait);
+        }     
+        keyboardSpawnerComponent.enabled = false;
+    }
+}
