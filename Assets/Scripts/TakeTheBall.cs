328af782516d727492ebb60b1c7a7f7554bb9929
aviv1620 <44925669+aviv1620@users.noreply.github.com>
2020-11-18 00:17:28 +0200

    spanner take ball

--- Assets/Scripts/TakeTheBall.cs
+++ Assets/Scripts/TakeTheBall.cs
@@ -8,16 +8,24 @@ public class TakeTheBall : MonoBehaviour {
     [SerializeField] string triggeringTag = "thrower";
     [Tooltip("key to press")]
     [SerializeField] protected KeyCode keyToPress;
+    [Tooltip("position outside the camera")]
+    [SerializeField] Vector3 positionOutsideTheCamera = new Vector3(-10,10,0);
+ [Tooltip("sprite spawners with ball")]
+    [SerializeField] Sprite spriteWithBall;
+    [Tooltip("sprite spawners without ball")]
+    [SerializeField] Sprite spriteWithOutBall;
 
     const float timeToWait = 0.0f;
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        var keyboardSpawnerComponent = other.GetComponent<KeyboardSpawner>();
-        var showBall = this.GetComponent<SpriteRenderer>();
+    private void OnTriggerEnter2D(Collider2D other) {        
+        var keyboardSpawnerComponent = other.GetComponent<KeyboardSpawner>();        
+        var show = other.GetComponent<SpriteRenderer>();
+        show.sprite = spriteWithBall;
+
         if (other.tag == triggeringTag) {           
-            showBall.enabled = false;
+            transform.position = positionOutsideTheCamera;                        
             if (keyboardSpawnerComponent) {
-                keyboardSpawnerComponent.StartCoroutine(BallTemporarily(keyboardSpawnerComponent, showBall));
+                keyboardSpawnerComponent.StartCoroutine(BallTemporarily(keyboardSpawnerComponent,show));
             }
         } else {
             Debug.Log("doesn't have KeyboardSpawner "+other.name);
@@ -25,14 +33,14 @@ public class TakeTheBall : MonoBehaviour {
     }
 
 
-    private IEnumerator BallTemporarily(KeyboardSpawner keyboardSpawnerComponent, SpriteRenderer showBall){
+    private IEnumerator BallTemporarily(KeyboardSpawner keyboardSpawnerComponent,SpriteRenderer show){
         keyboardSpawnerComponent.enabled = true;
         bool thrown = false;
-        while (!thrown) {
-            showBall.enabled = true;
+        while (!thrown) {            
             if (Input.GetKeyDown(keyToPress)){
                 thrown = true;
                 keyboardSpawnerComponent.enabled = false;
+                show.sprite = spriteWithOutBall;
             }
             yield return new WaitForSeconds(timeToWait);
         }     
